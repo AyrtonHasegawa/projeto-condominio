5d734f0a1d6026a478dc209ef1e78d3e1764f63e
Ayrton-Hasegawa-Sottelli <ayrton.hasegawa@sottelli.com>
2022-06-05 18:38:09 -0300

    Correção de Bug em Morador

---
+++ Gestao_Condominio/src/classe/FuncionarioAtual.java
@@ -0,0 +1,24 @@
+
+package classe;
+
+/**
+ *
+ * @author ayrton.hasegawa
+ */
+public class FuncionarioAtual {
+    String nome;
+    
+    public FuncionarioAtual() {
+        
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    
+}
